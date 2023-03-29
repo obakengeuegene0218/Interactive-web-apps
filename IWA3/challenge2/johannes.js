b1db56b2a6f5aa44ca93b5d80c70d3ec2512c02d
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-03-29 16:07:32 +0200

    IWA 3 challenges
    challenge 1 and 2 of IWA script and modules

---
+++ IWA3/challenge2/johannes.js
@@ -0,0 +1,8 @@
+//johannes.js
+
+export const firstname = "Johannes";
+export const surname = "Potgieter";
+export const role = "Intern";
+
+var display = firstname + " "+surname +' (' + role + ')'
+document.querySelector('#johannes').innerText = display
