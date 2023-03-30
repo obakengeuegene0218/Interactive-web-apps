be037f479ae2d81b5499432ae5d0ed8e962d4ab5
obakengeuegene0218 <125869768+obakengeuegene0218@users.noreply.github.com>
2023-03-30 13:34:38 +0200

    uploading debugged IWA4
    The IWA4 challenge is complete

---
+++ IWA4/student.js
@@ -0,0 +1,36 @@
+const date= 2050 ;
+let Status = 'parent';
+let count = 0;
+
+if (date) {
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+    let date = "April"
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	count = count + 4
+
+	if (Status == 'student') {
+	  console.log('June', 'Youth Day')
+		count = count + 1
+  }
+
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+     date = 'December'
+	console.log(date, 'Day of Reconciliation')
+	 count = count + 3
+
+	if (Status ='parent') {
+	  console.log(date, 'Christmas Day')
+		count = count + 1
+  }
+    console.log(date, 'Day of Goodwill')
+	count = count + 1
+}
+
+
+
+console.log('Your status is:', Status)
+console.log('The year is:', date)
+console.log('The total holidays is:', count)
